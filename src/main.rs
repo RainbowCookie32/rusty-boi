59276914f2a65514402940813f38e96af9bde248
Matias Moran <matimoran03@gmail.com>
2019-09-10 23:48:45 -0300

    Added log and simple_logger for better logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate sdl2;
+extern crate log;
+extern crate simple_logger;
 
 mod cpu;
 mod gpu;
@@ -10,10 +12,11 @@ mod emulator;
 mod opcodes;
 mod opcodes_prefixed;
 
+use log::info;
 
 fn main() {
-    
-    println!("Rusty Boi");
+
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+    info!("Rusty Boi");
     emulator::init_emu();
-    
 }
