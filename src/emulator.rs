5527c77ca8bdb50909479ef6cb11800a5037f7e9
Matias Moran <matimoran03@gmail.com>
2019-09-10 23:52:32 -0300

    Added SDL2 events handling, only Quit for now

--- src/emulator.rs
+++ src/emulator.rs
@@ -2,6 +2,9 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+use log::info;
+use log::error;
+
 use sdl2::pixels::Color;
 
 use super::cpu;
@@ -21,13 +24,13 @@ pub fn init_emu() {
     let bootrom: Vec<u8>;
     let rom: Vec<u8>;
     
-    println!("Point me to a Gameboy Bootrom");
+    info!("Point me to a GameBoy Bootrom");
 
     io::stdin().read_line(&mut bootrom_path).expect("Error while reading path to ROM");
     bootrom_path = bootrom_path.trim().to_string();
     bootrom = load_bootrom(bootrom_path);
 
-    println!("Point me to a GameBoy ROM");
+    info!("Point me to a GameBoy ROM");
 
     io::stdin().read_line(&mut rom_path).expect("Error while reading path to Bootrom");
     rom_path = rom_path.trim().to_string();
@@ -44,26 +47,37 @@ pub fn init_emu() {
 
 fn execution_loop(state: ConsoleState) {
 
+    let mut should_run = true;
+    
     let mut current_state = state;
-
     let mut cpu_result = cpu::CycleResult::Success;
 
     let sdl_context = sdl2::init().unwrap();
     let sdl_video = sdl_context.video().unwrap();
     let sdl_window = sdl_video.window("Rusty Boi", 160, 144).position_centered().build().unwrap();
     let mut sdl_canvas = sdl_window.into_canvas().build().unwrap();
+    let mut sdl_events = sdl_context.event_pump().unwrap();
 
     sdl_canvas.set_draw_color(Color::RGB(255, 255, 255));
     sdl_canvas.clear();
     sdl_canvas.present();
 
-    while cpu_result == cpu::CycleResult::Success {
+    while cpu_result == cpu::CycleResult::Success && should_run {
 
         cpu_result = cpu::exec_loop(&mut current_state.current_cpu, &mut current_state.current_memory);
         gpu::gpu_tick(&mut sdl_canvas, &mut current_state.current_gpu, &mut current_state.current_memory, &mut current_state.current_cpu.cycles.value);
+        
+        // Handle the Quit SDL event
+        for event in sdl_events.poll_iter() {
+            
+            match event {
+                sdl2::event::Event::Quit {..} => should_run = false,
+                _ => {},
+            }
+        }
     }
 
-    println!("Stopped CPU execution with state {:?}, stopping emulator", cpu_result);
+    info!("Stopped emulation. Last CPU state was '{:?}'.", cpu_result);
 }
 
 
@@ -73,8 +87,8 @@ fn load_bootrom(path: String) -> Vec<u8> {
     let mut data = Vec::new();
 
     match rom_file.read_to_end(&mut data){
-        Ok(_) => println!("Bootrom loaded"),
-        Err(_) => println!("Failed to open the Bootrom file"),
+        Ok(_) => info!("Bootrom loaded"),
+        Err(_) => error!("Failed to open the Bootrom file"),
     };
 
     data
@@ -86,8 +100,8 @@ fn load_rom(path: String) -> Vec<u8> {
     let mut data = Vec::new();
 
     match rom_file.read_to_end(&mut data){
-        Ok(_) => println!("ROM loaded"),
-        Err(_) => println!("Failed to open the ROM file"),
+        Ok(_) => info!("ROM loaded"),
+        Err(_) => error!("Failed to open the ROM file"),
     };
 
     data
