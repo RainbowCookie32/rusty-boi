bd1d20de68a4f5759ea83ac8b904a385d46209a3
Matias Moran <matimoran03@gmail.com>
2019-10-09 15:09:04 -0300

    gpu: remove the thread creation from gpu.rs
    It was a leftover from the initial MT implementation, it's now generated on emulator.rs

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,4 +1,3 @@
-use std::thread;
 use std::ops::Neg;
 use std::sync::mpsc::{Sender, Receiver};
 
@@ -41,198 +40,191 @@ pub struct GpuState {
 
 pub fn start_gpu(cpu_cycles: Receiver<u16>, memory: (Sender<MemoryAccess>, Receiver<GpuResponse>, Sender<bool>), input: Sender<InputEvent>) {
 
-    let initial_state = GpuState {
+    let mut current_state = GpuState {
         mode: 0,
         mode_clock: 0,
         line: 0,
         all_tiles: Vec::new(),
         background_points: Vec::new(),
-
         bg_dirty: false,
         tiles_dirty: false,
     };
-    
-    thread::spawn(move || {
+    let current_memory = (memory.0, memory.1);
         
-        let mut current_state = initial_state;
-        let current_memory = (memory.0, memory.1);
-        
-        let sdl_ctx = sdl2::init().unwrap();
-        let sdl_video = sdl_ctx.video().unwrap();
-        let mut sdl_events = sdl_ctx.event_pump().unwrap();
-        let emu_window = sdl_video.window("Rusty Boi", 160 * 3, 144 * 3).position_centered().build().unwrap();
-        let mut emu_canvas = emu_window.into_canvas().present_vsync().build().unwrap();
+    let sdl_ctx = sdl2::init().unwrap();
+    let sdl_video = sdl_ctx.video().unwrap();
+    let mut sdl_events = sdl_ctx.event_pump().unwrap();
+    let emu_window = sdl_video.window("Rusty Boi", 160 * 3, 144 * 3).position_centered().build().unwrap();
+    let mut emu_canvas = emu_window.into_canvas().present_vsync().build().unwrap();
 
-        // TODO: Add a way to change scaling without having to change it from code.
-        // Maybe as an argument, or request a scale multiplier after loading the ROMs.
-        emu_canvas.set_scale(3.0, 3.0).unwrap();
+    // TODO: Add a way to change scaling without having to change it from code.
+    // Maybe as an argument, or request a scale multiplier after loading the ROMs.
+    emu_canvas.set_scale(3.0, 3.0).unwrap();
+    emu_canvas.set_draw_color(Color::RGB(255, 255, 255));
+    emu_canvas.clear();
+    emu_canvas.present();
 
-        emu_canvas.set_draw_color(Color::RGB(255, 255, 255));
-        emu_canvas.clear();
-        emu_canvas.present();
+    loop {
 
-        loop {
+        for event in sdl_events.poll_iter() {
 
-            for event in sdl_events.poll_iter() {
-
-                match event {
-                    Event::Quit {..} => { input.send(InputEvent::Quit).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::A), .. } => { input.send(InputEvent::APressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::A), .. } => { input.send(InputEvent::AReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::S), .. } => { input.send(InputEvent::BPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::S), .. } => { input.send(InputEvent::BReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Up), .. } => { input.send(InputEvent::UpPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Up), .. } => { input.send(InputEvent::UpReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Left), .. } => { input.send(InputEvent::LeftPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Left), .. } => { input.send(InputEvent::LeftReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Right), .. } => { input.send(InputEvent::RightPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Right), .. } => { input.send(InputEvent::RightReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Down), .. } => { input.send(InputEvent::DownPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Down), .. } => { input.send(InputEvent::DownReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Return), .. } => { input.send(InputEvent::StartPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Return), .. } => { input.send(InputEvent::StartReleased).unwrap() },
-                    Event::KeyDown { keycode: Some(Keycode::Backspace), .. } => { input.send(InputEvent::SelectPressed).unwrap() },
-                    Event::KeyUp  { keycode: Some(Keycode::Backspace), .. } => { input.send(InputEvent::SelectReleased).unwrap() },
-                    _ => {}
-                }
-            }
-
-            let display_enabled: bool;
-            let mut if_value = memory_read(0xFF0F, &current_memory);
-
-            // This one isn't using memory_read in purpose.
-            // Having one full request on the beginning of the loop is useful so
-            // we can get the status of caches.
-            let mem_request = MemoryAccess {
-                operation: MemoryOp::Read,
-                address: 0xFF40,
-                value: 0,
-            };
-            let response: GpuResponse;
-
-            current_memory.0.send(mem_request).unwrap();
-            response = current_memory.1.recv().unwrap();
-            display_enabled = utils::check_bit(response.read_value, 7);
-
-            current_state.bg_dirty = response.background_dirty;
-            current_state.tiles_dirty = response.tiles_dirty;
-
-            if display_enabled {
-
-                current_state.mode_clock += cpu_cycles.recv().unwrap();
-
-                match current_state.mode {
-
-                    2 => {
-                        if current_state.mode_clock >= 80 {
-                            
-                            let mut stat_value = memory_read(0xFF41, &current_memory);
-                            
-                            current_state.mode_clock = 0;
-                            current_state.mode = 3;
-                            
-                            if utils::check_bit(stat_value, 5) {
-                                if_value = utils::set_bit(if_value, 2);
-                                memory_write(if_value, 0xFF0F, &current_memory);
-                            }
-
-                            stat_value = utils::set_bit(stat_value, 1);
-                            stat_value = utils::reset_bit(stat_value, 0);
-                            memory_write(stat_value, 0xFF41, &current_memory);
-                        }
-                    }
-
-                    3 => {
-                        if current_state.mode_clock >= 172 {
-                            
-                            let mut stat_value = memory_read(0xFF41, &current_memory);
-                            
-                            current_state.mode_clock = 0;
-                            current_state.mode = 0;
-
-                            stat_value = utils::set_bit(stat_value, 1);
-                            stat_value = utils::set_bit(stat_value, 0);
-                            memory_write(stat_value, 0xFF41, &current_memory);
-
-                            if current_state.tiles_dirty {
-                                make_tiles(&current_memory, &mut current_state);
-                                current_state.tiles_dirty = false;
-                            }
-
-                            if current_state.bg_dirty {
-                                make_background(&current_memory, &mut current_state);
-                                current_state.bg_dirty = false;
-                            }
-
-                            memory.2.send(false).unwrap();
-                        }
-                    }
-
-                    0 => {
-                        if current_state.mode_clock >= 204 {
-                
-                            let mut stat_value = memory_read(0xFF41, &current_memory);
-                            
-                            current_state.mode_clock = 0;
-                            current_state.line += 1;
-                            memory_write(current_state.line, 0xFF44, &current_memory);
-                            
-                            if utils::check_bit(stat_value, 3) {
-                                if_value = utils::set_bit(if_value, 2);
-                                memory_write(if_value, 0xFF0F, &current_memory);
-                            }
-
-                            stat_value = utils::reset_bit(stat_value, 1);
-                            stat_value = utils::reset_bit(stat_value, 0);
-                            memory_write(stat_value, 0xFF41, &current_memory);
-
-                            if current_state.all_tiles.len() >= 128 && current_state.background_points.len() >= 65536
-                            {
-                                draw(&mut current_state, &mut emu_canvas, &current_memory);
-                            }
-
-                            if current_state.line == 144 {
-                                // Go into VBlank mode.
-                                current_state.mode = 1;
-                                // Send data to screen.
-                                emu_canvas.present();
-                            }
-                        }
-                    }
-        
-                    // VBlank mode
-                    1 => {
-                        if current_state.mode_clock >= 456 {
-
-                            let mut stat_value = memory_read(0xFF41, &current_memory);
-                            
-                            current_state.mode_clock = 0;
-                            current_state.line += 1;
-                            memory_write(current_state.line, 0xFF44, &current_memory);
-                            
-                            if_value = utils::set_bit(if_value, 0);
-                            memory_write(if_value, 0xFF0F, &current_memory);
-
-                            stat_value = utils::reset_bit(stat_value, 1);
-                            stat_value = utils::set_bit(stat_value, 0);
-                            memory_write(stat_value, 0xFF41, &current_memory);
-
-                            if current_state.line == 154 {
-                            // End of the screen, restart.
-                                current_state.mode = 2;
-                                current_state.line = 1;
-                                
-                                memory_write(current_state.line, 0xFF44, &current_memory);
-                                emu_canvas.clear();
-                            }
-                        }
-                    }
-
-                    _ => panic!("Invalid GPU Mode"),
-                }
+            match event {
+                Event::Quit {..} => { input.send(InputEvent::Quit).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => { input.send(InputEvent::APressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::A), .. } => { input.send(InputEvent::AReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => { input.send(InputEvent::BPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::S), .. } => { input.send(InputEvent::BReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => { input.send(InputEvent::UpPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Up), .. } => { input.send(InputEvent::UpReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => { input.send(InputEvent::LeftPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Left), .. } => { input.send(InputEvent::LeftReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => { input.send(InputEvent::RightPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Right), .. } => { input.send(InputEvent::RightReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => { input.send(InputEvent::DownPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Down), .. } => { input.send(InputEvent::DownReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Return), .. } => { input.send(InputEvent::StartPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Return), .. } => { input.send(InputEvent::StartReleased).unwrap() },
+                Event::KeyDown { keycode: Some(Keycode::Backspace), .. } => { input.send(InputEvent::SelectPressed).unwrap() },
+                Event::KeyUp  { keycode: Some(Keycode::Backspace), .. } => { input.send(InputEvent::SelectReleased).unwrap() },
+                _ => {}
             }
         }
-    });
+
+        let display_enabled: bool;
+        let mut if_value = memory_read(0xFF0F, &current_memory);
+
+        // This one isn't using memory_read in purpose.
+        // Having one full request on the beginning of the loop is useful so
+        // we can get the status of caches.
+        let mem_request = MemoryAccess {
+            operation: MemoryOp::Read,
+            address: 0xFF40,
+            value: 0,
+        };
+        let response: GpuResponse;
+
+        current_memory.0.send(mem_request).unwrap();
+        response = current_memory.1.recv().unwrap();
+        display_enabled = utils::check_bit(response.read_value, 7);
+
+        current_state.bg_dirty = response.background_dirty;
+        current_state.tiles_dirty = response.tiles_dirty;
+
+        if display_enabled {
+
+            current_state.mode_clock += cpu_cycles.recv().unwrap();
+
+            match current_state.mode {
+
+                2 => {
+                    if current_state.mode_clock >= 80 {
+                            
+                        let mut stat_value = memory_read(0xFF41, &current_memory);
+                            
+                        current_state.mode_clock = 0;
+                        current_state.mode = 3;
+                            
+                        if utils::check_bit(stat_value, 5) {
+                            if_value = utils::set_bit(if_value, 2);
+                            memory_write(if_value, 0xFF0F, &current_memory);
+                        }
+
+                        stat_value = utils::set_bit(stat_value, 1);
+                        stat_value = utils::reset_bit(stat_value, 0);
+                        memory_write(stat_value, 0xFF41, &current_memory);
+                    }
+                }
+
+                3 => {
+                    if current_state.mode_clock >= 172 {
+                            
+                        let mut stat_value = memory_read(0xFF41, &current_memory);
+                            
+                        current_state.mode_clock = 0;
+                        current_state.mode = 0;
+
+                        stat_value = utils::set_bit(stat_value, 1);
+                        stat_value = utils::set_bit(stat_value, 0);
+                        memory_write(stat_value, 0xFF41, &current_memory);
+
+                        if current_state.tiles_dirty {
+                            make_tiles(&current_memory, &mut current_state);
+                            current_state.tiles_dirty = false;
+                        }
+
+                        if current_state.bg_dirty {
+                            make_background(&current_memory, &mut current_state);
+                            current_state.bg_dirty = false;
+                        }
+
+                        memory.2.send(false).unwrap();
+                    }
+                }
+
+                0 => {
+                    if current_state.mode_clock >= 204 {
+                
+                        let mut stat_value = memory_read(0xFF41, &current_memory);
+                            
+                        current_state.mode_clock = 0;
+                        current_state.line += 1;
+                        memory_write(current_state.line, 0xFF44, &current_memory);
+                            
+                        if utils::check_bit(stat_value, 3) {
+                            if_value = utils::set_bit(if_value, 2);
+                            memory_write(if_value, 0xFF0F, &current_memory);
+                        }
+
+                        stat_value = utils::reset_bit(stat_value, 1);
+                        stat_value = utils::reset_bit(stat_value, 0);
+                        memory_write(stat_value, 0xFF41, &current_memory);
+
+                        if current_state.all_tiles.len() >= 128 && current_state.background_points.len() >= 65536
+                        {
+                            draw(&mut current_state, &mut emu_canvas, &current_memory);
+                        }
+
+                        if current_state.line == 144 {
+                            // Go into VBlank mode.
+                            current_state.mode = 1;
+                            // Send data to screen.
+                            emu_canvas.present();
+                        }
+                    }
+                }
+        
+                // VBlank mode
+                1 => {
+                    if current_state.mode_clock >= 456 {
+
+                        let mut stat_value = memory_read(0xFF41, &current_memory);
+                            
+                        current_state.mode_clock = 0;
+                        current_state.line += 1;
+                        memory_write(current_state.line, 0xFF44, &current_memory);
+                            
+                        if_value = utils::set_bit(if_value, 0);
+                        memory_write(if_value, 0xFF0F, &current_memory);
+
+                        stat_value = utils::reset_bit(stat_value, 1);
+                        stat_value = utils::set_bit(stat_value, 0);
+                        memory_write(stat_value, 0xFF41, &current_memory);
+
+                        if current_state.line == 154 {
+                        // End of the screen, restart.
+                            current_state.mode = 2;
+                            current_state.line = 1;
+                                
+                            memory_write(current_state.line, 0xFF44, &current_memory);
+                            emu_canvas.clear();
+                        }
+                    }
+                }
+
+                _ => panic!("Invalid GPU Mode"),
+            }
+        }
+    }
 }
 
 fn draw(state: &mut GpuState, canvas: &mut Canvas<Window>, memory: &(Sender<MemoryAccess>, Receiver<GpuResponse>)) {
@@ -302,7 +294,7 @@ fn make_tile(bytes: &Vec<u8>) -> Tile {
 
         // If both bytes are zero, then we won't have colors since all bits will be 0.
         // Just skip checking them if that's the case and move on to the next ones.
-        if false /*tile_bytes[0] == 0 && tile_bytes[1] == 0*/ {
+        if tile_bytes[0] == 0 && tile_bytes[1] == 0 {
             generated_colors += 8;
             color_index += 8;
         }
