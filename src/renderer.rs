24caa6d58ddc373349d6720b84363a66a73473f2
Matias Moran <matimoran03@gmail.com>
2019-10-16 22:39:27 -0300

    ui: show info from the header when selecting a rom

--- src/renderer.rs
+++ src/renderer.rs
@@ -12,7 +12,9 @@ use imgui_opengl_renderer;
 use log::error;
 
 use std::io;
+use std::io::Read;
 use std::fs;
+use std::fs::File;
 use std::sync::mpsc;
 use std::path::PathBuf;
 
@@ -24,6 +26,7 @@ struct State {
     pub emu_running: bool,
     pub booted_rom: PathBuf,
     pub game_scale: f32,
+    pub header_data: HeaderData,
 }
 
 struct ImguiSys {
@@ -32,12 +35,27 @@ struct ImguiSys {
     pub renderer: imgui_opengl_renderer::Renderer,
 }
 
+struct HeaderData {
+    title: String,
+    publisher: String,
+    cart_type: String,
+    rom_size: String,
+    ram_size: String,
+}
+
 pub fn init_renderer() {
 
     let mut emu_state = State {
         emu_running: false,
         booted_rom: PathBuf::new(),
         game_scale: 1.0,
+        header_data: HeaderData {
+            title: String::from(""),
+            publisher: String::from(""),
+            cart_type: String::from(""),
+            rom_size: String::from(""),
+            ram_size: String::from(""),
+        }
     };
     
     // Init SDL
@@ -171,11 +189,12 @@ fn ui_loop(sys: &mut ImguiSys, window: &video::Window, mouse_state: &sdl2::mouse
 
                     if MenuItem::new(&filename).build_with_ref(&imgui_ui, &mut false) { 
 
+                        emu.header_data = parse_header(&file.path());
                         if PathBuf::from("Bootrom.gb").exists() {
                             emu.emu_running = true;
                             emu.booted_rom = file.path();
                         }
-                    };
+                    }
                 }
             }
             else {
@@ -183,6 +202,14 @@ fn ui_loop(sys: &mut ImguiSys, window: &video::Window, mouse_state: &sdl2::mouse
             }
             menu.end(&imgui_ui);
         }
+        imgui_ui.separator();
+
+        imgui_ui.text(format!("ROM Title: {}", &emu.header_data.title));
+        imgui_ui.text(format!("Publisher: {}", &emu.header_data.publisher));
+        imgui_ui.text(format!("Cart Type: {}", &emu.header_data.cart_type));
+        imgui_ui.text(format!("ROM Size: {}", &emu.header_data.rom_size));
+        imgui_ui.text(format!("RAM Size: {}", &emu.header_data.ram_size));
+
         imgui_ui.separator();
         if PathBuf::from("Bootrom.gb").exists() {
             imgui_ui.text_colored([0.0, 1.0, 0.0, 1.0], im_str!("Bootrom located, everything's ready"));
@@ -201,6 +228,122 @@ fn ui_loop(sys: &mut ImguiSys, window: &video::Window, mouse_state: &sdl2::mouse
 
     sys.renderer.render(imgui_ui);
     window.gl_swap_window();
+}
+
+
+fn parse_header(file_path: &PathBuf) -> HeaderData {
+
+    let header: HeaderData;
+    let mut file = File::open(file_path).unwrap();
+    let mut header_buffer = [0; 335];
+    file.read(&mut header_buffer).unwrap();
+
+    let game_title = String::from_utf8(header_buffer[308..323].to_vec()).unwrap().replace("?", " ");
+    println!("{}", game_title);
+    let lic_code = match header_buffer[331] {
+
+        0x00 => String::from("None"),
+        0x01 => String::from("Nintendo R&D 1"),
+        0x08 => String::from("Capcom"),
+        0x13 => String::from("Electronic Arts"),
+        0x18 => String::from("Hudson Soft"),
+        0x19 => String::from("b-ai"),
+        0x20 => String::from("kss"),
+        0x22 => String::from("pow"),
+        0x24 => String::from("PCM Complete"),
+        0x25 => String::from("san-z"),
+        0x28 => String::from("Kemco Japan"),
+        0x29 => String::from("seta"),
+        0x30 => String::from("Viacom"),
+        0x31 => String::from("Nintendo"),
+        0x32 => String::from("Bandai"),
+        0x33 => String::from("Ocean/Acclaim"),
+        0x34 => String::from("Konami"),
+        0x35 => String::from("Hector"),
+        0x37 => String::from("Taito"),
+        0x38 => String::from("Hudson"),
+        0x39 => String::from("Banpresto"),
+        0x41 => String::from("Ubi Soft"),
+        0x42 => String::from("Atlus"),
+        0x44 => String::from("Malibu"),
+        0x46 => String::from("angel"),
+        0x47 => String::from("Bullet-Proof"),
+        0x49 => String::from("irem"),
+        0x50 => String::from("Absolute"),
+        0x51 => String::from("Acclaim"),
+        0x52 => String::from("Activision"),
+        0x53 => String::from("American sammy"),
+        0x54 => String::from("Konami"),
+        0x55 => String::from("Hi tech entertainment"),
+        0x56 => String::from("LJN"),
+        0x57 => String::from("Matchbox"),
+        0x58 => String::from("Mattel"),
+        0x59 => String::from("Milton Bradley"),
+        0x60 => String::from("Titus"),
+        0x61 => String::from("Virgin"),
+        0x64 => String::from("LucasArts"),
+        0x67 => String::from("Ocean"),
+        0x69 => String::from("Electronic Arts"),
+        0x70 => String::from("Infogrames"),
+        0x71 => String::from("Interplay"),
+        0x72 => String::from("Broderbund"),
+        0x73 => String::from("sculptured"),
+        0x75 => String::from("sci"),
+        0x78 => String::from("THQ"),
+        0x79 => String::from("Accolade"),
+        0x80 => String::from("misawa"),
+        0x83 => String::from("Iozc"),
+        0x86 => String::from("tokuma shoten i*"),
+        0x87 => String::from("tsukuda ori*"),
+        0x91 => String::from("Chunsoft"),
+        0x92 => String::from("Video system"),
+        0x93 => String::from("Ocean/Acclaim"),
+        0x95 => String::from("Varie"),
+        0x96 => String::from("Yonezawa/s'pal"),
+        0x97 => String::from("Kaneko"),
+        0x99 => String::from("Pack in soft"),
+        0xA4 => String::from("Konami (Yu-Gi-Oh!)"),
+        _ => String::from("Unknown"),
+    };
+
+    let mbc_type = match header_buffer[327] {
+        0x00 => String::from("No MBC"),
+        0x01 => String::from("MBC1"),
+        0x02 => String::from("MBC1 with RAM"),
+        0x03 => String::from("MBC1 with RAM and battery"),
+        0x05 => String::from("MBC2"),
+        0x06 => String::from("MBC2 with battery"),
+        0x08 => String::from("ROM with RAM"),
+        0x09 => String::from("ROM with RAM and battery"),
+        0x0B => String::from("MMM01"),
+        0x0C => String::from("MMM01 with RAM"),
+        0x0D => String::from("MMM01 with RAM and battery"),
+        0x0F => String::from("MBC3 with timer and battery"),
+        0x11 => String::from("MBC3"),
+        0x12 => String::from("MBC3 with RAM"),
+        0x13 => String::from("MBC3 with RAM and battery"),
+        _ => String::from("Unknown"),
+    };
+
+    let rom_size = format!("{}KB", 32 << header_buffer[328]);
+    let ram_size = match header_buffer[329] {
+        0x00 => String::from("None"),
+        0x01 => String::from("2KB"),
+        0x02 => String::from("8KB"),
+        0x03 => String::from("32KB"),
+        0x04 => String::from("128KB"),
+        0x05 => String::from("64KB"),
+        _ => String::from("Unknown"),
+    };
+
+    header = HeaderData {
+        title: game_title,
+        publisher: lic_code,
+        cart_type: mbc_type,
+        rom_size: rom_size,
+        ram_size: ram_size,
+    };
+    header
 }
 
 fn get_all_roms() -> Vec<fs::DirEntry> {
