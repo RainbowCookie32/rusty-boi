f9a188c19b1c158ba02491dcff96dfaf3b637dd2
Matias Moran <matimoran03@gmail.com>
2019-10-31 15:41:11 -0300

    timer: fixes and cleanups
    The DIV register should be incremented independently of the timer being enabled or not. Fixes the blocks in Tetris

--- src/timer.rs
+++ src/timer.rs
@@ -26,27 +26,27 @@ pub fn timer_cycle(timer_state: &mut TimerState, cycles: u16, memory: &Arc<Mutex
 
     let tac_value = memory::timer_read(0xFF07, memory);
     let timer_enabled = utils::check_bit(tac_value, 2);
-    let mut current_state = timer_state;
+
+    timer_state.div_cycles += cycles;
+
+    if timer_state.div_cycles >= 256 {
+
+        let div_value = memory::timer_read(0xFF04, memory);
+        let new_value = div_value.overflowing_add(1).0;
+        memory::timer_write(0xFF04, new_value, memory);
+        timer_state.div_cycles = 0;
+    }
 
     if timer_enabled {
             
-        current_state.needed_cycles = get_frequency(tac_value);
-        current_state.timer_cycles += cycles;
-        current_state.div_cycles += cycles;
+        timer_state.needed_cycles = get_frequency(tac_value);
+        timer_state.timer_cycles += cycles;
 
-        if current_state.div_cycles >= 256 {
-
-            let div_value = memory::timer_read(0xFF04, memory);
-            let new_value = div_value.overflowing_add(1);
-            memory::timer_write(0xFF04, new_value.0, memory);
-            current_state.div_cycles = 0;
-        }
-
-        if current_state.timer_cycles >= current_state.needed_cycles {
+        if timer_state.timer_cycles >= timer_state.needed_cycles {
 
             let tima_value = memory::timer_read(0xFF05, memory);
             let new_value = tima_value.overflowing_add(1);
-            current_state.timer_cycles = 0;
+            timer_state.timer_cycles = 0;
 
             if new_value.1 {
                     
@@ -64,22 +64,13 @@ pub fn timer_cycle(timer_state: &mut TimerState, cycles: u16, memory: &Arc<Mutex
 
 fn get_frequency(tac: u8) -> u16 {
 
-    let bit0 = utils::check_bit(tac, 0);
-    let bit1 = utils::check_bit(tac, 1);
-
-    if !bit0 && !bit1 {
-        1024
-    }
-    else if !bit0 && bit1{
-        16
-    }
-    else if bit0 && !bit1 {
-        64
-    }
-    else if bit0 && bit1 {
-        256
-    }
-    else {
-        0
+    let value = tac & 3;
+    
+    match value {
+        0 => 1024,
+        1 => 16,
+        2 => 64,
+        3 => 256,
+        _ => 0,
     }
 }
